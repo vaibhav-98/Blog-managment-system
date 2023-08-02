821d04bab3a3ab8e62b53214ab1a38dd761f595f
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-08-02 14:27:28 +0530

    uplode files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require ('mongoose');
+mongoose.set('strictQuery', true)
+const app = express()
+app.use(express.json())
+
+
+
+mongoose.connect("mongodb+srv://sanhil143:raisahab12345@sanhildb.kk3knyj.mongodb.net/group16Database")
+.then( ()  => console.log("My MongoDB is connected"))
+.catch( (err) => console.log(err))
+
+
+app.use('/ping',(req,res) =>{
+    res.send("pong")
+} )
+
+
+app.listen(3000, () => {
+    console.log("Express app running on port" + 3000);
+})
